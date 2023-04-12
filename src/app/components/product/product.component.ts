5d291165c208cdaf693c842160dee2e3f74d5b85
Albert Yakunin <albertyakunin13@gmail.com>
2023-04-13 00:35:48 +0300

    add products.ts

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,7 @@
+import { Component, ComponentRef } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+})
+export class ProductComponent {}
