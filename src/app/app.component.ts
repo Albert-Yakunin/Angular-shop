1031a673d8f5da1b87e602226e3cbacac4e761ee
Albert Yakunin <a.yakunin@cleverdev-software.com>
2023-04-16 17:16:58 +0300

    added error service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Iproduct } from './models/product';
 import { products as data } from './data/products';
+import { ProductService } from './services/product.service';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular Shop';
-  products:Iproduct[] = data;
+  // products: Iproduct[];
+  loading = false;
+  products$: Observable<Iproduct[]>;
+
+  constructor(private productsService: ProductService) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.products$ = this.productsService
+      .getAll()
+      .pipe(tap(() => (this.loading = false)));
+    // this.productsService.getAll().subscribe((products) => {
+    //   this.products = products;
+    //   this.loading = false;
+    // });
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,29 @@
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, delay, throwError } from 'rxjs';
+import { Iproduct } from '../models/product';
+import { ErrorService } from './error.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ProductService {}
+export class ProductService {
+  private errorHandler(error: HttpErrorResponse) {
+    this.errorService.handle(error.message);
+    return throwError(() => error.message);
+  }
+
+  constructor(private http: HttpClient, private errorService: ErrorService) {}
+
+  getAll(): Observable<Iproduct[]> {
+    return this.http
+      .get<Iproduct[]>('https://fakestoreapi.com/products', {
+        params: new HttpParams().append('limit', 5),
+      })
+      .pipe(delay(500), catchError(this.errorHandler));
+  }
+}
